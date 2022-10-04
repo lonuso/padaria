e6b00106eb9ca5cd27604626a99500cb7b5cdeae
Louise <louisensoares@gmail.com>
2022-10-04 14:02:50 -0300

    Comentando a notação service

--- padaria/padaria/src/main/java/com/louise/padaria/service/FuncionarioService.java
+++ padaria/padaria/src/main/java/com/louise/padaria/service/FuncionarioService.java
@@ -3,7 +3,7 @@ package com.louise.padaria.service;
 import com.louise.padaria.model.Funcionario;
 import com.louise.padaria.repository.FuncionarioRepository;
 import org.springframework.stereotype.Service;
-
+//Service -> anotaçãoque indica que essa classe é uma classe de serviço
 @Service
 public class FuncionarioService {
     private FuncionarioRepository repository;
