5aa1392c2e21ed86583197b20ff9acc0e3ced6bc
Louise <louisensoares@gmail.com>
2022-10-05 14:03:31 -0300

    Criação da serviço de funcionario

--- padaria/padaria/src/main/java/com/louise/padaria/service/FuncionarioService.java
+++ padaria/padaria/src/main/java/com/louise/padaria/service/FuncionarioService.java
@@ -2,11 +2,38 @@ package com.louise.padaria.service;
 
 import com.louise.padaria.model.Funcionario;
 import com.louise.padaria.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 //Service -> anotaçãoque indica que essa classe é uma classe de serviço
 @Service
 public class FuncionarioService {
+    //Autowired -> é responsavel por fazer a instanciação do repository
+    @Autowired
     private FuncionarioRepository repository;
+
+    public boolean salvarFuncionario(Funcionario funcionario){
+        repository.save(funcionario);
+        return true;
+    }
+
+    public boolean deletarFuncionario(Funcionario f){
+        Funcionario funcionario = buscarFuncionarioPorId(f.getId());
+        if(funcionario != null){
+            repository.delete(f);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean atualizarFuncionario(Funcionario func){
+        Funcionario funcionario = buscarFuncionarioPorId(func.getId());
+        if (funcionario != null){
+            repository.save(func);
+            return true;
+        }
+        return false;
+
+    }
 
     public Funcionario buscarFuncionarioPorId(Integer id){
         Funcionario funcionario = repository.findById(id).get();
