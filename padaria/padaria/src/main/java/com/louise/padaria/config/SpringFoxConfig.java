031cbb266e540df1d8ad99a2769e3af978ed9908
Louise <louisensoares@gmail.com>
2022-10-21 20:17:14 -0300

    Personalizando meus dados de criação

--- padaria/padaria/src/main/java/com/louise/padaria/config/SpringFoxConfig.java
+++ padaria/padaria/src/main/java/com/louise/padaria/config/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.louise.padaria.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +21,18 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.louise.padaria"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfoMetaData());
+
+    }
+    private ApiInfo apiInfoMetaData() {
+
+        return new ApiInfoBuilder().title("padaria")
+                .description("Api padaria estudo SpringBoot e Swagger")
+                .contact(new Contact("Louise", "", "louisensoares@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
                 .build();
     }
 }
